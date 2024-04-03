a8cb7f0b66aeef8c977cfff632a85d551e30dd1c
Eric P. Odom <epkeaton@hotmail.com>
2024-04-03 09:14:24 -0400

    request info as a modal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function checkStickyElementPosition(stickyElementId) {
     const stickyElement = document.getElementById(stickyElementId);
     const footer = document.querySelector('footer');
-    const nav = document.querySelector('nav'); // Add this line to select the navigation element
+    const nav = document.querySelector('nav'); // Added this line to select the navigation element
 
     // Create an Intersection Observer with a rootMargin of 10px
     const observer = new IntersectionObserver((entries) => {
@@ -15,14 +15,44 @@ function checkStickyElementPosition(stickyElementId) {
     }, {
         root: null,
         rootMargin: '5px',
-        threshold: 0,
+        threshold: 0
     });
 
     // Observe both the footer and the navigation
     observer.observe(footer);
-    observer.observe(nav); // Add this line to observe the navigation
+    observer.observe(nav); // Added this line to observe the navigation
+
+    // Check initial position on document load
+    if (footer.getBoundingClientRect().top <= window.innerHeight) {
+        stickyElement.classList.add('stuck');
+    }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     checkStickyElementPosition('sticky-cta');
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const infoModal = document.querySelector('.info-modal');
+    const infoModalInnerClose = document.querySelector('.info-modal--inner-close');
+
+    // Show info modal on button click
+    document.querySelectorAll('.btn-info').forEach((button) => {
+        button.addEventListener('click', function (event) {
+            event.preventDefault();
+            infoModal.classList.add('active');
+        });
+    });
+
+    // Hide info modal on inner close button click
+    infoModalInnerClose.addEventListener('click', function () {
+        infoModal.classList.remove('active');
+    });
+
+    // Hide info modal when clicking outside of it
+    infoModal.addEventListener('click', function (event) {
+        if (event.target !== this) return;
+        infoModal.classList.remove('active');
+    });
 });
